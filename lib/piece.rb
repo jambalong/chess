a0bd4716bbf2dcb2ff9930065825beaea915b0de
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-07-08 13:12:23 -0700

    Add Pawn moves and capture

--- lib/piece.rb
+++ lib/piece.rb
@@ -6,10 +6,21 @@
 # It should also keep track of whether the piece has moved.
 # It should be subclassed for each type of piece (e.g., Pawn, Rook, Knight).
 class Piece
+  attr_reader :color, :position, :has_moved
+
   def initialize(color, position)
     @color = color
     @position = position
     @has_moved = false
+  end
+
+  def move_to(new_position)
+    @position = new_position
+    @has_moved = true
+  end
+
+  def valid_move?(end_pos, board)
+    raise NotImplementedError
   end
 
   private
--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -8,4 +8,50 @@ class Pawn < Piece
     super(color, position)
     @symbol = color == :white ? '♙' : '♟︎'
   end
+
+  def valid_move?(end_pos, board)
+    return false if obstructed?(end_pos, board)
+    return false unless forward_move?(end_pos) || capture_move?(end_pos)
+
+    true
+  end
+
+  private
+
+  def obstructed?(end_pos, board)
+    return false if @position[1] == end_pos[1] && forward_one?(end_pos) && board[end_pos[0]][end_pos[1]].nil?
+    return false if @position[1] == end_pos[1] && forward_two?(end_pos) && board[end_pos[0]][end_pos[1]].nil?
+
+    return false if capture_move?(end_pos) && !board[end_pos[0]][end_pos[1]].nil?
+
+    true
+  end
+
+  def forward_move?(end_pos)
+    return true if @position[1] == end_pos[1] && forward_one?(end_pos)
+    return true if @position[1] == end_pos[1] && forward_two?(end_pos)
+
+    false
+  end
+
+  def forward_one?(end_pos)
+    return true if @color == :white && @position[0] + 1 == end_pos[0]
+    return true if @color == :black && @position[0] - 1 == end_pos[0]
+
+    false
+  end
+
+  def forward_two?(end_pos)
+    return true if @color == :white && @position[0] == 1 && @position[0] + 2 == end_pos[0]
+    return true if @color == :black && @position[0] == 6 && @position[0] - 2 == end_pos[0]
+
+    false
+  end
+
+  def capture_move?(end_pos)
+    return true if @color == :white && @position[0] + 1 == end_pos[0] && (@position[1] - 1 == end_pos[1] || @position[1] + 1 == end_pos[1])
+    return true if @color == :black && @position[0] - 1 == end_pos[0] && (@position[1] - 1 == end_pos[1] || @position[1] + 1 == end_pos[1])
+
+    false
+  end
 end
