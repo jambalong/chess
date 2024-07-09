c4397cea899d4d17d88d7f1e84a184d2a09d074b
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-07-09 12:21:15 -0700

    Add King moves

--- lib/piece/king.rb
+++ lib/piece/king.rb
@@ -8,4 +8,24 @@ class King < Piece
     super(color, position)
     @symbol = color == :white ? '♔' : '♚'
   end
+
+  def valid_move?(end_pos, board)
+    return false unless super
+    return false if move_distance_invalid?(end_pos)
+
+    true
+  end
+
+  private
+
+  def move_distance_invalid?(end_pos, far_threshold: 1, short_threshold: 1)
+    row_diff = (@position[0] - end_pos[0]).abs
+    col_diff = (@position[1] - end_pos[1]).abs
+
+    too_far = row_diff > far_threshold || col_diff > far_threshold
+    too_short = row_diff < short_threshold && col_diff < short_threshold
+    sideways = row_diff > far_threshold && col_diff > far_threshold
+
+    too_far || too_short || sideways
+  end
 end
