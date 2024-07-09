b9b3548056f7a9e5c3e54c68c592c9158460e78f
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-07-08 18:19:06 -0700

    Add Knight moves

--- lib/piece/knight.rb
+++ lib/piece/knight.rb
@@ -8,4 +8,24 @@ class Knight < Piece
     super(color, position)
     @symbol = color == :white ? '♘' : '♞'
   end
+
+  def valid_move?(end_pos, board)
+    return false unless l_move?(end_pos)
+    return false unless within_board?(end_pos, board)
+    return false if same_color_piece?(end_pos, board)
+
+    true
+  end
+
+  private
+
+  def l_move?(end_pos)
+    x_diff = (@position[0] - end_pos[0]).abs
+    y_diff = (@position[1] - end_pos[1]).abs
+
+    return true if x_diff == 2 && y_diff == 1
+    return true if x_diff == 1 && y_diff == 2
+
+    false
+  end
 end
