66e3a552b07b45b26d369cd24a5909f2581b46c6
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-07-09 11:13:46 -0700

    Add Rook moves

--- lib/piece/rook.rb
+++ lib/piece/rook.rb
@@ -8,4 +8,58 @@ class Rook < Piece
     super(color, position)
     @symbol = color == :white ? '♖' : '♜'
   end
+
+  def valid_move?(end_pos, board)
+    return false if obstructed?(end_pos, board)
+    return false if same_color_piece?(end_pos, board)
+    return false unless horizontal_move?(end_pos) || vertical_move?(end_pos)
+
+    true
+  end
+
+  private
+
+  def obstructed?(end_pos, board)
+    if same_row?(end_pos)
+      obstructed_in_row?(end_pos, board)
+    elsif same_column?(end_pos)
+      obstructed_in_column?(end_pos, board)
+    else
+      false
+    end
+  end
+
+  def horizontal_move?(end_pos)
+    @position[0] == end_pos[0]
+  end
+
+  def vertical_move?(end_pos)
+    @position[1] == end_pos[1]
+  end
+
+  def same_row?(end_pos)
+    @position[0] == end_pos[0]
+  end
+
+  def same_column?(end_pos)
+    @position[1] == end_pos[1]
+  end
+
+  def obstructed_in_row?(end_pos, board)
+    start_col, end_col = [@position[1], end_pos[1]].sort
+    (start_col + 1...end_col).each do |col|
+      return true if board.piece_at([@position[0], col])
+    end
+    false
+  end
+
+  def obstructed_in_column?(end_pos, board)
+    start_row, end_row = [@position[0], end_pos[0]].sort
+    (start_row + 1...end_row).each do |row|
+      return true if board.piece_at([row, @position[1]])
+    end
+    false
+  end
+
+
 end
